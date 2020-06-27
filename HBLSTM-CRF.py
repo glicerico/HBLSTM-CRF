1b2f8d847ab7cfd12404a72b97abdded2538ab49
ahwang16 <ahh2143@columbia.edu>
2020-06-27 11:59:35 -0400

    Import tf v1 and replace toy dataset

--- HBLSTM-CRF.py
+++ HBLSTM-CRF.py
@@ -1,5 +1,5 @@
 import numpy as np 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import tensorflow_addons as tfa
 import time
 # from swda_data import load_file
@@ -198,6 +198,13 @@ def main():
     tf.reset_default_graph()
     config = tf.ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = 0.4
+
+    data = [[[1,2,3,4],[1,2,3],[2,3,5]],[[1,0], [4]],[[1,2,8,4],[1,1,3],[2,3,9,1,3,1,9]], [[1,2,3,4,5,7,8,9],[9,1,2,4],[8,9,0,1,2]],[[1,2,4,3,2,3],[9,8,7,5,5,5,5,5,5,5,5]],[[1,2,3,4,5,6,9],[9,1,0,0,2,4,6,5,4]],[[1,2,3,4,5,6,7,8,9],[9,1,2,4],[8,9,0,1,2]],[[1]] , [[1,2,11,2,3,2,1,1,3,4,4], [6,5,3,2,1,1,4,5,6,7], [9,8,1], [1,6,4,3,5,7,8], [0,9,2,4,6,2,4,6], [5,2,2,5,6,7,3,7,2,2,1], [0,0,0,1,2,7,5,3,7,5,3,6], [1,3,6,6,3,3,3,5,6,7,2,4,2,1], [1,2,4,5,2,3,1,5,1,1,2], [9,0,1,0,0,1,3,3,5,3,2], [0,9,2,3,0,2,1,5,5,6], [9,0,0,1,4,2,4,10,13,11,12], [0,0,1,2,3,0,1,1,0,1,2], [0,0,1,3,1,12,13,3,12,3], [0,9,1,2,3,4,1,3,2]]]
+    labels = [[1,2,1],[0, 3],[1,2,1],[1,0,2], [2,1], [1,1], [2,1,2], [4], [0,1,2,0,2,4,2,1,0,1,0,2,1,2,0]]
+    train_data = data[:6]
+    train_labels = labels[:6]
+    dev_data = data[6:]
+    dev_labels = labels[6:]
     
     with tf.Session(config=config) as sess:
         model = DAModel()
@@ -211,8 +218,6 @@ def main():
             for train_batch_dialogues, train_batch_labels in minibatches(train_data, train_labels, batchSize):
                 _, train_batch_dialogue_lengths = pad_sequences(train_batch_dialogues, 0)
                 train_batch_word_ids, train_batch_utterance_lengths = pad_sequences(train_batch_dialogues, 0, nlevels=2)
-                print(train_batch_dialogues)
-                print(train_batch_word_ids)
                 true_labs = train_batch_labels
                 train_batch_true_labels, _ = pad_sequences(true_labs, 0)
                 counter += 1
@@ -269,7 +274,3 @@ def main():
                     dev_loss_summ.value.add(tag='dev_loss', simple_value=valid_loss)
                     writer.add_summary(dev_loss_summ, counter)
                     print("counter = {}, dev_loss = {}, dev_accuacy = {}".format(counter, valid_loss, valid_accuracy))
-
-            
-if __name__ == "__main__":
-    main()
